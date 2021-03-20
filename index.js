f05db7e37b88ce2cbdc92910a590665e745be930
Jadaoi06 <jadaoingram@gmail.com>
2021-03-20 14:36:22 +0000

    Tested javascript

---
+++ index.js
@@ -0,0 +1,9 @@
+//We need to add certain tags to the HTML to get user input
+//'Get input from HTML file into JS file - linking JS and HTML together
+//Put the rules of FizzBuzz in our JS
+// If input is divisible by 3, print 'Fizz'
+// If input is divisble by 5, print 'Buzz'
+// If input is divisible by 3 and 5, print 'FizBuzz'
+// Have someone test our code - play our game
+
+function myFunction () {console.log('Testing, testing 1,2,3');}
